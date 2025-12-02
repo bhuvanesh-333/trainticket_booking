39f1eaa190ba37f08dfb6b058231886ef90975a5
Bhuvanesh <bhuvanprabha4@gmail.com>
2025-12-02 16:05:49 +0530

    Delete OneDrive/Desktop/React/insurance-policy-system directory

--- OneDrive/Desktop/React/insurance-policy-system/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders home page welcome message', () => {
-  render(<App />);
-  const welcomeElement = screen.getByText(/welcome to insurance policy system!/i);
-  expect(welcomeElement).toBeInTheDocument();
-});
--- OneDrive/Desktop/React/insurance-policy-system/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-const Footer = () => (
-  <footer className="footer">
-    <p>&copy; 2025 Insurance Co. All Rights Reserved.</p>
-  </footer>
-);
-
-export default Footer;
